6b0436ad6cb68889dd0c2f7c94e404f65e8a3cdf
Jeff Risberg <jrisberg@aol.com>
2014-08-02 17:15:44 -0700

    fixed the routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
-devise_for :users
+  devise_for :users
 
-root to: 'main#index'
+  root to: 'main#index'
 
-get "main/questions_1" => "main#questions_1"
+  get "main/questions_1" => "main#questions_1"
 
+  get "main/questions_2" => "main#questions_2"
+
+  get "main/recommendations" => "main#recommendations"
 
 end
